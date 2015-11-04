1a431dda4e05f655ddbf89ac20790bd9e483025f
Armin Ronacher <armin.ronacher@active-4.com>
2015-11-04 20:20:48 +0100

    Basic UI support

---
+++ gui/static/js/app.jsx
@@ -0,0 +1,278 @@
+'use strict';
+
+import remote from 'remote';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ipc from 'ipc';
+import shell from 'shell';
+
+import Component from './components/Component';
+import { LektorInterop } from './lektorInterop';
+import i18n from './i18n';
+
+const app = remote.require('app');
+const dialog = remote.require('dialog');
+
+i18n.setLanguageFromLocale(app.getLocale());
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      lektorState: 'initializing',
+      projectPath: null,
+      projectState: 'closed',
+      projectData: null,
+      projectServer: null,
+      projectServerStatus: [],
+      buttonTitle: null
+    };
+
+    this.onRequestOpenFiles = this.onRequestOpenFiles.bind(this);
+    this.onBeforeUnload = this.onBeforeUnload.bind(this);
+    this.lektorInterop = new LektorInterop();
+  }
+
+  openProject(path) {
+    this.closeProject();
+    this.setState({
+      projectPath: path,
+      projectState: 'loading',
+      projectData: null,
+      projectServerStatus: []
+    }, () => {
+      this.lektorInterop.analyzeProject(path).then((project) => {
+        if (project === null) {
+          this.setState({
+            projectState: 'failed',
+          });
+        } else {
+          this.setState({
+            projectState: 'loaded',
+            projectData: project
+          });
+          this.spawnServerForProject(project.project_path);
+        }
+      }, (failure) => {
+        this.setState({
+          projectState: 'failed'
+        });
+        dialog.showErrorBox(i18n.trans('FAILED_TO_LAUNCH_LEKTOR'),
+                            failure.toString());
+      });
+    });
+  }
+
+  browseForProject() {
+    dialog.showOpenDialog(null, {
+      title: 'Open Lektor Project',
+      filters: [
+        {
+          name: 'Lektor Projects',
+          extensions: ['lektorproject']
+        }
+      ],
+      properties: ['openFile']
+    }, (files) => {
+      if (files) {
+        this.openProject(files[0]);
+      }
+    });
+  }
+
+  closeProject() {
+    if (this.state.projectServer) {
+      this.state.projectServer.shutdown();
+    }
+    this.setState({
+      projectPath: null,
+      projectState: 'closed',
+      projectData: null,
+      projectServerStatus: []
+    });
+  }
+
+  browseWebsite() {
+    let server = this.state.projectServer;
+    if (server) {
+      shell.openExternal(server.getUrl());
+    }
+  }
+
+  viewAdminPanel() {
+    let server = this.state.projectServer;
+    if (server) {
+      shell.openExternal(server.getAdminUrl());
+    }
+  }
+
+  spawnServerForProject(projectPath) {
+    if (this.state.projectServer !== null) {
+      this.state.projectServer.shutdown();
+    }
+    this.state.projectServer = this.lektorInterop.spawnServer(projectPath, {
+      statusLineCallback: (statusLine) => {
+        this.setState({
+          projectServerStatus: Array.prototype.concat.call(
+            this.state.projectServerStatus, statusLine)
+        });
+      }
+    });
+  }
+
+  uiIsLocked() {
+    return this.state.projectState === 'loading';
+  }
+
+  componentDidMount() {
+    super.componentDidMount();
+    ipc.on('requestOpenFiles', this.onRequestOpenFiles);
+    window.addEventListener('beforeunload', this.onBeforeUnload);
+    this.resizeWindow();
+    this.lektorInterop.checkLektor()
+      .then((version) => {
+        this.setState({
+          lektorState: 'initialized'
+        });
+      }, (failure) => {
+        this.setState({
+          lektorState: 'failed'
+        });
+        dialog.showErrorBox(i18n.trans('FAILED_TO_LAUNCH_LEKTOR'),
+                            failure.toString());
+        app.quit();
+      });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('beforeunload', this.onBeforeUnload);
+    ipc.removeListener('requestOpenFiles', this.onRequestOpenFiles);
+    super.componentWillUnmount();
+  }
+
+  componentDidUpdate() {
+    super.componentDidUpdate();
+    if (this.refs.log) {
+      this.refs.log.scrollTop = this.refs.log.scrollHeight;
+    }
+    this.resizeWindow();
+  }
+
+  resizeWindow() {
+    let win = remote.getCurrentWindow();
+    let [width, height] = win.getSize();
+    let app = this.refs.app;
+    win.setSize(width, app.scrollHeight);
+  }
+
+  onRequestOpenFiles(pathsToOpen) {
+    if (pathsToOpen.length == 1) {
+      this.openProject(pathsToOpen[0]);
+    }
+  }
+
+  onBeforeUnload(event) {
+    let server = this.state.projectServer;
+    if (server !== null) {
+      server.shutdown();
+    }
+  }
+
+  showButtonTitle(title) {
+    this.setState({
+      buttonTitle: title
+    });
+  }
+
+  renderNav() {
+    let rv = [];
+    var uiLock = this.uiIsLocked();
+
+    let addButton = (state, icon, title, callback) => {
+      var disabled = uiLock;
+      if (state !== null && this.state.projectState === state) {
+        disabled = true;
+      }
+
+      rv.push(
+        <button
+          className="list-group-item"
+          disabled={disabled}
+          onClick={callback}
+          onMouseMove={this.showButtonTitle.bind(this, title)}
+          onMouseLeave={this.showButtonTitle.bind(this, null)}
+          key={rv.length}><i className={'fa fa-' + icon}></i></button>
+      );
+    };
+
+    addButton(null, 'folder-open-o', i18n.trans('OPEN_PROJECT'), this.browseForProject.bind(this));
+    addButton('closed', 'times', i18n.trans('CLOSE_PROJECT'), this.closeProject.bind(this));
+    addButton('closed', 'eye', i18n.trans('BROWSE_WEBSITE'), this.browseWebsite.bind(this));
+    addButton('closed', 'pencil', i18n.trans('VIEW_ADMIN_PANEL'), this.viewAdminPanel.bind(this));
+    addButton(null, 'power-off', i18n.trans('QUIT'), () => { app.quit(); });
+
+    return rv;
+  }
+
+  renderProjectStatus() {
+    if (this.state.projectState === 'closed') {
+      return null;
+    } else if (this.state.projectState === 'failed') {
+      return <p>{i18n.trans('FAILED_TO_LOAD_PROJECT')}</p>;
+    } else if (this.state.projectState === 'loading') {
+      return <p>{i18n.trans('LOADING_PROJECT')}</p>;
+    }
+    return (
+      <div className="project-status">
+        <dl>
+          <dt>{i18n.trans('PROJECT')}</dt>
+          <dd>{this.state.projectData.name}</dd>
+        </dl>
+        <ul className="log" ref="log">{
+          this.state.projectServerStatus.map((item, idx) => {
+            return <li key={idx}>{item}</li>;
+          })
+        }</ul>
+      </div>
+    );
+  }
+
+  renderLektorInit() {
+    let spinner = null;
+    if (this.state.lektorState === 'initializing') {
+      spinner = <p className="spinner">
+        <i className="fa fa-spin fa-spinner fa-3x"></i>
+      </p>;
+    }
+    return (
+      <div className="app app-initializing" ref="app">
+        <p>{i18n.trans('INITIALIZING_LEKTOR')}</p>
+        {spinner}
+      </div>
+    );
+  }
+
+  render() {
+    if (this.state.lektorState !== 'initialized') {
+      return this.renderLektorInit();
+    }
+    return (
+      <div className="app" ref="app">
+        <h1>Lektor</h1>
+        <div className="main-nav">
+          <div className="list-group">
+            {this.renderNav()}
+          </div>
+          <p className="explanation">
+            {this.state.buttonTitle}
+          </p>
+        </div>
+        {this.renderProjectStatus()}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ gui/static/js/lektorInterop.jsx
@@ -0,0 +1,161 @@
+import remote from 'remote';
+import path from 'path';
+import fs from 'fs';
+import childProcess from 'child_process';
+
+import i18n from './i18n';
+
+const app = remote.require('app');
+const dialog = remote.require('dialog');
+
+
+function getResourceFolder() {
+  // XXX: windows support
+  return path.dirname(app.getAppPath());
+}
+
+function findBundledLektorExecutable() {
+  let res = getResourceFolder();
+  try {
+    let macExe = path.join(res, 'lektor-dist', 'lektor');
+    fs.accessSync(macExe, fs.X_OK);
+    return macExe;
+  } catch (e) {
+    return null;
+  }
+}
+
+function findGlobalLektorExecutable() {
+  // XXX: windows support
+  let rv = childProcess.spawnSync('which', ['lektor']);
+  if (rv.status === 0) {
+    return (rv.output[1] + '').trim();
+  }
+  return null;
+}
+
+export function findLektorExecutable() {
+  return findBundledLektorExecutable() || findGlobalLektorExecutable();
+}
+
+class LektorServer {
+  constructor(child, options) {
+    this._child = child;
+    this.state = 'starting';
+    this.options = options;
+    this._statusLineCallback = options.statusLineCallback;
+
+    child.stdout.on('data', (data) => {
+      var lines = (data + '').split(/\r?\n/);
+      lines.forEach((line) => {
+        this._statusLineCallback(line.trimRight());
+      });
+    });
+
+    child.on('close', (code) => {
+      this._statusLineCallback('Server shut down with code ' + code);
+    });
+  }
+
+  getUrl() {
+    return 'http://localhost:' + this.options.port + '/';
+  }
+
+  getAdminUrl() {
+    return this.getUrl() + 'admin/';
+  }
+
+  shutdown() {
+    this._child.kill('SIGHUP');
+  }
+}
+
+function spawnLektor(exe, args) {
+  var env = {};
+  Object.keys(process.env).forEach((key) => {
+    env[key] = process.env[key];
+  });
+  env.LEKTOR_RUN_FROM_UI = '1';
+  env.LEKTOR_RESOURCES = getResourceFolder();
+  env.LEKTOR_UI_LANG = i18n.currentLanguage;
+
+  return childProcess.spawn(exe, args, {env: env});
+}
+
+
+export class LektorInterop {
+
+  constructor() {
+    this._lektorExecutable = null;
+  }
+
+  getLektorExecutable() {
+    if (this._lektorExecutable !== null) {
+      return this._lektorExecutable;
+    }
+    return this._lektorExecutable = findLektorExecutable();
+  }
+
+  /* Loads lektor once to pre-initialize it (this will ensure that it
+     unpacks the pex) and it also gives us back the version number. */
+  checkLektor() {
+    let exe = this.getLektorExecutable();
+    return new Promise((resolve, reject) => {
+      if (!exe) {
+        return reject(new Error('Cannot locate Lektor executable'));
+      }
+
+      let child = spawnLektor(exe, ['--version']);
+      let buf = '';
+      child.stdout.on('data', (data) => {
+        buf += data;
+      });
+      child.on('close', () => {
+        let match = buf.match(/\s+version\s+(.*?)\s*$/i);
+        if (match) {
+          resolve(match[1]);
+        } else {
+          reject(new Error('Failed to launch Lektor executable'));
+        }
+      });
+    });
+  }
+
+  /* given a path to a project this analyzes the project there and returns
+     the data as object.  Since this loading can take time it's returned as
+     a promise */
+  analyzeProject(path) {
+    let exe = this.getLektorExecutable();
+    return new Promise((resolve, reject) => {
+      if (!exe) {
+        return reject(new Error('Cannot locate Lektor executable'));
+      }
+
+      let child = spawnLektor(
+        exe, ['--project', path, 'project-info', '--json']);
+
+      let buf = '';
+      child.stdout.on('data', (data) => {
+        buf += data;
+      });
+
+      child.on('close', (code) => {
+        if (code === 0) {
+          resolve(JSON.parse(buf));
+        } else {
+          resolve(null);
+        }
+      });
+    });
+  }
+
+  /* spawns the server for a project */
+  spawnServer(projectPath, options) {
+    options.port = options.port || 5000;
+    let exe = this.getLektorExecutable();
+    let child = spawnLektor(
+      exe, ['--project', projectPath, 'devserver', '--port',
+            options.port + '']);
+    return new LektorServer(child, options);
+  }
+}
