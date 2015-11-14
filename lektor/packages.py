16dab7df64bb18f8d7ebe5fcbf935f22629d8ba8
Armin Ronacher <armin.ronacher@active-4.com>
2015-11-14 22:38:21 +0100

    Added support for local plugin loading.

--- lektor/packages.py
+++ lektor/packages.py
@@ -4,68 +4,167 @@ import site
 import errno
 import click
 import shutil
+import tempfile
 import pkg_resources
 
 from .utils import portable_popen
 
 
-def pip_install(package_root, package, version):
+def download_and_install_package(package_root, package=None, version=None,
+                                 requirements_file=None):
+    """This downloads and installs a specific version of a package."""
+    # XXX: windows
     env = dict(os.environ)
-    rv = portable_popen([
+
+    args = [
         sys.executable,
         '-m', 'pip', 'install', '--target', package_root,
-        '%s==%s' % (package, version)
-    ], env=env).wait()
+    ]
+
+    if package is not None:
+        args.append('%s%s%s' % (package, version and '==' or '',
+                                version or ''))
+    if requirements_file is not None:
+        args.extend(('-r', requirements_file))
+
+    rv = portable_popen(args, env=env).wait()
     if rv != 0:
         raise RuntimeError('Failed to install dependency package.')
 
 
+def install_local_package(package_root, path):
+    """This installs a local dependency of a package."""
+    # XXX: windows
+    env = dict(os.environ)
+    env['PYTHONPATH'] = package_root
+
+    # Step 1: generate egg info and link us into the target folder.
+    rv = portable_popen([
+        sys.executable,
+        '-m', 'pip',
+        'install', '--editable', path,
+        '--install-option=--install-dir=%s' % package_root,
+        '--no-deps'
+    ], env=env).wait()
+    if rv != 0:
+        raise RuntimeError('Failed to install local package')
+
+    # Step 2: generate the egg info into a temp folder to find the
+    # requirements.
+    tmp = tempfile.mkdtemp()
+    try:
+        rv = portable_popen([
+            sys.executable,
+            'setup.py', '--quiet', 'egg_info', '--quiet',
+            '--egg-base', tmp
+        ], cwd=path).wait()
+        dirs = os.listdir(tmp)
+        if rv != 0 or len(dirs) != 1:
+            raise RuntimeError('Failed to create egg info for local package.')
+        requires = os.path.join(tmp, dirs[0], 'requires.txt')
+
+        # We have dependencies, install them!
+        if os.path.isfile(requires):
+            download_and_install_package(package_root,
+                                         requirements_file=requires)
+    finally:
+        shutil.rmtree(tmp)
+
+
 def load_manifest(filename):
-    rv = []
+    rv = {}
     try:
         with open(filename) as f:
             for line in f:
+                if line[:1] == '@':
+                    rv[line.strip()] = None
+                    continue
                 line = line.strip().split('=', 1)
                 if len(line) == 2:
-                    rv.append((line[0].strip(), line[1].strip()))
+                    key = line[0].strip()
+                    value = line[1].strip()
+                    rv[key] = value
     except IOError as e:
         if e.errno != errno.ENOENT:
             raise
-    return dict(rv)
+    return rv
 
 
 def write_manifest(filename, packages):
     with open(filename, 'w') as f:
         for package, version in sorted(packages.items()):
-            f.write('%s=%s\n' % (package, version))
+            if package[:1] == '@':
+                f.write('%s\n' % package)
+            else:
+                f.write('%s=%s\n' % (package, version))
 
 
-def update_cache(package_root, packages):
+def list_local_packages(path):
+    """Lists all local packages below a path that could be installed."""
+    rv = []
+    try:
+        for filename in os.listdir(path):
+            if os.path.isfile(os.path.join(path, filename, 'setup.py')):
+                rv.append('@' + filename)
+    except OSError:
+        pass
+    return rv
+
+
+def update_cache(package_root, remote_packages, local_package_path):
+    """Updates the package cache at package_root for the given dictionary
+    of packages as well as packages in the given local package path.
+    """
     manifest_file = os.path.join(package_root, 'lektor-packages.manifest')
+    local_packages = list_local_packages(local_package_path)
 
-    version_changed = False
-    all_packages = sorted(packages.items())
-    old_packages = load_manifest(manifest_file)
+    requires_wipe = False
+    old_manifest = load_manifest(manifest_file)
     to_install = []
 
-    for package, version in all_packages:
-        old_version = old_packages.pop(package, None)
+    all_packages = dict(remote_packages)
+    all_packages.update((x, None) for x in local_packages)
+
+    # step 1: figure out which remote packages to install.
+    for package, version in remote_packages.iteritems():
+        old_version = old_manifest.pop(package, None)
         if old_version is None:
             to_install.append((package, version))
         elif old_version != version:
-            version_changed = True
+            requires_wipe = True
 
-    # If we have any old packages that we need to remove or any version
-    # changed, we wipe the entire package cache.
-    if old_packages or version_changed:
+    # step 2: figure out which local packages to install
+    for package in local_packages:
+        if old_manifest.pop(package, False) is False:
+            to_install.append((package, None))
+
+    # Bad news, we need to wipe everything
+    if requires_wipe or old_manifest:
         shutil.rmtree(package_root)
-        to_install = all_packages
+        to_install = all_packages.items()
 
     if to_install:
         click.echo('Updating packages in %s for project' % package_root)
         for package, version in to_install:
-            pip_install(package_root, package, version)
-        write_manifest(manifest_file, packages)
+            if package[:1] == '@':
+                install_local_package(package_root,
+                    os.path.join(local_package_path, package[1:]))
+            else:
+                download_and_install_package(package_root, package, version)
+        write_manifest(manifest_file, all_packages)
+
+
+def add_site(path):
+    """This adds a path to as proper site packages to all associated import
+    systems.  Primarily it invokes `site.addsitedir` and also configures
+    pkg_resources' metadata accordingly.
+    """
+    site.addsitedir(path)
+    ws = pkg_resources.working_set
+    ws.entry_keys.setdefault(path, [])
+    ws.entries.append(path)
+    for dist in pkg_resources.find_distributions(path, False):
+        ws.add(dist, path, insert=True)
 
 
 def load_packages(env):
@@ -79,6 +178,6 @@ def load_packages(env):
     """
     config = env.load_config()
     package_root = env.project.get_package_cache_path()
-    update_cache(package_root, config['PACKAGES'])
-    site.addsitedir(package_root)
-    pkg_resources.working_set.add_entry(package_root)
+    update_cache(package_root, config['PACKAGES'],
+                 os.path.join(env.root_path, 'packages'))
+    add_site(package_root)
