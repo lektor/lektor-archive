7b470356ccd3828c3ac8a6c37bdf4cd483bd0c98
Armin Ronacher <armin.ronacher@active-4.com>
2015-11-27 21:23:34 +0100

    Make example be the quickstart output

--- example/packages/demo/lektor_demo.py
+++
@@ -1,9 +0,0 @@
-from lektor.pluginsystem import Plugin
-
-
-class DemoPlugin(Plugin):
-    name = 'Demo plugin'
-    description = 'This is a demo plugin'
-
-    def on_process_template_context(self, context, **extra):
-        context['demo_value_from_plugin'] = 42
